c15202c82094e9e2548694435de8e99ed3e2432b
K11810 <fastofall6545@gmail.com>
2019-07-09 10:16:22 +0000

    022-step14-3_Add: add testcode for deadline order and deadline-column entry

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -3,10 +3,12 @@ FactoryBot.define do
     factory :task do
       title { 'test_task_01' }
       content { 'testtesttest' }
+      deadline { '2019-12-31'}
     end
 
     factory :second_task, class: Task do
       title { 'test_task_02' }
       content { 'samplesample' }
+      deadline { '2019-11-10'}
     end
   end
